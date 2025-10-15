dc3da503876a3d0fbc00f6e1666a5f3cdaa48240
Rene Miranda <renemirandau4@gmail.com>
2025-10-14 23:17:38 -0600

    chore: remove models.ts

--- src1/models.ts
+++
@@ -1,7 +0,0 @@
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-
-export const llm = new ChatGoogleGenerativeAI({
-  model: "gemini-2.5-flash",
-  temperature: 0.5,
-  apiKey: process.env.GEMINI_API_KEY!,
-});
